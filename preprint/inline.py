726ded287049ba15029924ac7a89402aca1a48a9
Jonathan Sick <jonathansick@mac.com>
2014-01-03 18:30:20 -0700

    Create functions in inline to open git blobs
    Adds support for reading and inlining inputs of latex documents in the
    git tree. GitPython (0.3) is used to access git blobs. Blobs are encoded
    as utf-8 unicode text objects.

--- preprint/inline.py
+++ preprint/inline.py
@@ -3,14 +3,24 @@
 """
 Inline \input{*} latex files. Useful for packaging and latexdiff.
 
-Inspired by
+There are groups of functions: tools for working with latex files on the
+regular filesystem, and functions for working with files embedded as
+blobs in the git tree.
 
-- http://dropbearcode.blogspot.ca/2011/09/multiple-file-latex-diff.html
-- http://stackoverflow.com/questions/393843/python-and-regular-expression-with-unicode
+For files on the file system:
+
+- :func:`inline` and :func:`_sub_inline`.
+
+For files in the git tree:
+
+- :func:`read_git_blob` to read text from a git blob
+- :func:`inline_blob` to inline text from files in the git tree.
 """
 
 import re
 import codecs
+import git
+import os
 
 
 def inline(root_text):
@@ -46,6 +56,76 @@ def _sub_line(match):
     # Recursively inline files
     included_text = inline(included_text)
     return included_text
+
+
+def inline_blob(commit_ref, root_text):
+    """Inline all input latex files that exist as git blobs in a tree object.
+    
+    The inlining is accomplished recursively.
+
+    All files are opened as UTF-8 unicode files.
+
+    Parameters
+    ----------
+    commit_ref : str
+        String identifying a git commit/tag.
+    path : str
+        Path of file to process in the tree
+
+    Returns
+    -------
+    txt : str
+        Text with referenced files included.
+    """
+    def _sub_blob(match):
+        """Function to be used with re.sub to inline files for each match."""
+        fname = match.group(1)
+        if not fname.endswith('.tex'):
+            full_fname = ".".join((fname, 'tex'))
+        else:
+            full_fname = fname
+        included_text = read_git_blob(commit_ref, full_fname)
+        # Recursively inline files
+        included_text = inline_blob(commit_ref, included_text)
+        return included_text
+    input_pattern = re.compile(ur'\\input{(.*)}', re.UNICODE)
+    result = input_pattern.sub(_sub_blob, root_text)
+    return result
+
+
+def read_git_blob(commit_ref, path):
+    """Get text from a git blob."""
+    repo = git.Repo('.')
+    tree = repo.tree(commit_ref)
+    dirname, fname = os.path.split(path)
+    if dirname == '':
+        text =_read_blob(tree, fname)
+    else:
+        components = path.split(os.sep)
+        text = _read_blob_in_tree(tree, components)
+    return text
+
+
+def _read_blob_in_tree(tree, components):
+    """Recursively open trees to ultimately read a blob"""
+    if len(components) == 1:
+        # Tree is direct parent of blob
+        return _read_blob(tree, components[0])
+    else:
+        # Still trees to open
+        dirname = components.pop(0)
+        for t in tree.trees:
+            if t.name == dirname:
+                return _read_blob_in_tree(t, components)
+
+
+def _read_blob(tree, filename):
+    for blb in tree.blobs:
+        if blb.name == filename:
+            txt = unicode(blb.data_stream.read(), 'utf-8')
+            # txt = txt.encode('utf-8')
+            return txt
+    return None
 
 
 def main():
